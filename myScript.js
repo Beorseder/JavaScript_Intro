08ec3fcf154cf308259a2094c77a12c7278dd0f3
Beorseder <45007766+Beorseder@users.noreply.github.com>
2019-01-07 10:21:52 -0700

    Yay changes

--- myScript.js
+++ myScript.js
@@ -13,25 +13,24 @@ function demoChange() {
     
     return;
 }
-
-//Defining variables that I'm not using
-
-var x = 5;
-var y = 6;
-var z = x + y;
-
 //This function changes the text in the math paragraph
-
-function mathChange() {
-    var textOne = "something like \"What is 5 + 6?\"";
-    var textTwo = "11";
+function mathChange(x, y, calc) {
+    var textOne = "something like \"What is " + x + " + " + y + "?\"";
+    var textTwo = Number(x) + Number(y);
+    calc = Boolean(calc);
     
-    if (document.getElementById('math').innerHTML === textOne) {
-        document.getElementById('math').innerHTML = textTwo;
+    if (calc) {
+        if (document.getElementById('math').innerHTML !== textOne) {
+            document.getElementById('math').innerHTML = textOne;
+        }
+        else {
+            document.getElementById('math').innerHTML = textTwo;
+        }
     }
     else {
         document.getElementById('math').innerHTML = textOne;
     }
+    
     
     return;
 }
